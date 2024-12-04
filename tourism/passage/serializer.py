cb333c905f2577a2aceb4af8f6672568f1938ccb
Tastes_like_joy <ru00012r@gmail.com>
2024-12-04 22:18:28 +0300

    законченный файл сериализатора

--- tourism/passage/serializer.py
+++ tourism/passage/serializer.py
@@ -7,6 +7,24 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['email', 'phone', 'fam', 'name', 'otc']
 
+    def save(self, **kwargs):
+
+        if self.is_valid():
+            user = User.objects.filter(pk=self.validated_data.get('pk'))
+
+        if user.exists():
+            return user.first()
+        else:
+            new_user = User.objects.create(
+                email=self.validated_data.get('email'),
+                phone=self.validated_data.get('phone'),
+                fam=self.validated_data.get('fam'),
+                name=self.validated_data.get('name'),
+                otc=self.validated_data.get('otc')
+            )
+            return new_user
+
+
 
 class CoordinatesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -31,12 +49,29 @@ class PassagesSerializer(serializers.HyperlinkedModelSerializer):
     coord = CoordinatesSerializer()
     lvl = LevelsSerializer()
     img = ImagesSerializer()
+    add_time = serializers.DateTimeField(format='%d %m %Y %H:%M:%S', read_only=True)
+    status = serializers.CharField(read_only=True)
 
     class Meta:
         model = Passages
         fields = ['title', 'beauty_title', 'other_title', 'connect']
 
+    def validate(self, data):
 
+        if self.instance:
+            user = self.instance.user
+            data_user = data['user']
+            validation_fields = [
+                user.email == data_user['email'],
+                user.phone == data_user['phone'],
+                user.fam == data_user['fam'],
+                user.name == data_user['name'],
+                user.otc == data_user['otc']
+            ]
+
+            if data_user and not all(validation_fields):
+                 raise serializers.ValidationError('Отклонено: Данные пользователя нельзя изменить.')
+            return data
 
 
 
